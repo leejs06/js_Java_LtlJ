be487b9de062ac59cf3198d1a4fe16130552b3e1
Leejiseong06 <leejs06@soleo.kr>
2024-11-13 15:10:57 +0900

    11/13 commit

---
+++ src/extends1/super1/Parrent.java
@@ -0,0 +1,10 @@
+package extends1.super1;
+
+public class Parrent {
+
+    public String value = "Parent";
+
+    public void hello() {
+        System.out.println("Parrent.hello");
+    }
+}
--- src/final1/FinalFieldMain.java
+++ src/final1/FinalFieldMain.java
@@ -22,5 +22,7 @@ public class FinalFieldMain {
         //상수
         System.out.println("상수");
         System.out.println(FieldInit.CONST_VALUE);
+
+
     }
 }
