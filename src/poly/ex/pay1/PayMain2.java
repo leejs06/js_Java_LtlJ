3f105be6d20b55ea895c33cbd657e5d808835d7b
Leejiseong06 <leejs06@soleo.kr>
2024-11-18 14:46:53 +0900

    11/18 commit PM

---
+++ src/poly/ex/pay1/PayMain2.java
@@ -0,0 +1,27 @@
+package poly.ex.pay1;
+
+import java.util.Scanner;
+
+public class PayMain2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PayService payService = new PayService();
+
+        while (true) {
+            System.out.println("결제 수단을 입력하세요: ");
+            String pay0ption = scanner.nextLine();
+
+            if (pay0ption.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            System.out.println("결제 금액을 입력하세요 : ");
+            int amount = scanner.nextInt();
+            scanner.nextLine();
+
+            payService.processPay(pay0ption, amount);
+        }
+    }
+}
